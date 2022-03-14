a7bd0022a3f9e5c41f42cdd5a6e6154da27b7428
Klamkin, Michael <mike@klamkin.com>
2022-03-14 14:01:15 -0400

    fix imports, add folder structure

--- misc.py
+++ misc.py
@@ -1,6 +1,6 @@
 import pickle, hashlib, json
 from typing import Dict, Any
-from condor_tune import AUTOTUNE_DIR, TRIAL_DIR
+from condor_tune import TUNE_DIR, TRIAL_DIR
 
 class Torch_CPU_Unpickler(pickle.Unpickler):
     # see https://github.com/pytorch/pytorch/issues/16797#issuecomment-633423219
@@ -24,9 +24,47 @@ def dict_hash(dictionary: Dict[str, Any]) -> str:
 
 def move_trials():
     import shutil, os
-    # move all contents of autotune/trials/* to autotune/old
+    # move all contents of tune/trials/* to tune/old
     for dir in os.listdir(TRIAL_DIR):
         # NOTE: if dir already exists in old, we remove it and overwrite
-        if os.path.exists(f"{AUTOTUNE_DIR}/old/{dir}"):
-            shutil.rmtree(f"{AUTOTUNE_DIR}/old/{dir}")
-        shutil.move(f"{TRIAL_DIR}/{dir}", f"{AUTOTUNE_DIR}/old/{dir}")
+        if os.path.exists(f"{TUNE_DIR}/old/{dir}"):
+            shutil.rmtree(f"{TUNE_DIR}/old/{dir}")
+        shutil.move(f"{TRIAL_DIR}/{dir}", f"{TUNE_DIR}/old/{dir}")
+
+
+## resulting/expected folder structure:
+# condor-tune-project
+# ├── results
+# │   ├── condor_tune_2022-03-14_01-15-45.pkl         (pickled results DataFrame)
+# │   ├── ...
+# ├── tune                                            (TUNE_DIR)
+# │   ├── train.sh                                    (script which launches training)
+# │   ├── metric.sh                                   (script which launches metric calculation)
+# │   ├── trials                                      (TRIAL_DIR)
+# │   │   ├── 0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a0a  (hash of this trial's params)
+# │   │   │   ├── flows                               (flows directory, created by training script)
+# │   │   │   │   ├── results                         (post-flow values)
+# │   │   │   │   │   ├── 1.json                      (flow results file)
+# │   │   │   │   │   ├── 2.json                      (flow results file)
+# │   │   │   │   │   ├── ...
+# │   │   │   │   │   └── n.json                      (flow results file)
+# │   │   │   │   ├── 1.json                          (flow input file, created by training script)
+# │   │   │   │   ├── 2.json                          (flow input file, created by training script)
+# │   │   │   │   ├── ...
+# │   │   │   │   └── n.json                          (flow input file, created by training script)
+# │   │   │   ├── params.json                         (params for this run, chosen by HyperOpt)
+# │   │   │   ├── train.log                           (training log file from HTCondor)
+# │   │   │   ├── train.out                           (train stdout file from HTCondor)
+# │   │   │   ├── train.err                           (train stderr file from HTCondor)
+# │   │   │   ├── metric.log                          (metric log file from HTCondor)
+# │   │   │   ├── metric.out                          (metric stdout file from HTCondor)
+# │   │   │   ├── metric.err                          (metric stderr file from HTCondor)
+# │   │   │   ├── training_done                       (flag indicating training finished successfully)
+# │   │   │   ├── run_flow.cmd                        (command used to run flow job array, created by training script)
+# │   │   │   ├── training_results.pkl                (pickled training results, including model weights and metrics)
+# │   │   │   └── results.pkl                         (pickled final results, created by metric script)
+# │   │   ├── ...
+# │   ├── old
+# │   │   ├── 1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b1b  (old trial hash)
+# │   │   │   └── ... (same structure as above)
+# │   │   └── ...
